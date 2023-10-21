78c0ff502154c3a3da5ae45875a7a9a6a367203a
msdq17 <mabdullahsadiq299@gmail.com>
2023-10-21 15:55:28 +0100

    finished search bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,3 +10,16 @@
 <!-- {:else} -->
   <!-- <Login /> -->
 <!-- {/if} -->
+
+<!-- Loading content -->
+
+<!-- body wrapper -->
+<div class="w-full relative pt-[175px] min-h-screen pl-[20px]">
+    <!-- grid wrapper -->
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 sm:gap-8">
+        <div class="blurback h-[200px] w-[200px] "></div>
+        <div class="blurback h-[200px] w-[200px]"></div>
+        <div class="blurback h-[200px] w-[200px]"></div>
+        <div class="blurback h-[200px] w-[200px]"></div>
+    </div>
+</div>
