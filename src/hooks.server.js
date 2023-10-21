35343e6e8dc33b9aa19b578dfe553f8ec779d90b
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-10-21 15:26:00 +0100

    test scopes

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -6,7 +6,13 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
 export const handle = SvelteKitAuth({
   providers: [
-    Spotify({ clientId: SPOTIFY_ID, clientSecret: SPOTIFY_SECRET })
+    Spotify({ 
+      clientId: SPOTIFY_ID, 
+      clientSecret: SPOTIFY_SECRET, 
+      authorization: {
+        url: "https://accounts.spotify.com/authorize",
+        params: {scope: "playlist-read-private app-remote-control user-top-read playlist-modify-public playlist-modify-private streaming user-modify-playback-state"}
+      }})
   ], 
   adapter: MongoDBAdapter(clientPromise)
 })
