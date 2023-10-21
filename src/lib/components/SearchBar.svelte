981d6428163d751e246d0bf62d817a2d9e588bf7
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-10-21 18:45:24 +0100

    please

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -8,17 +8,39 @@
         left: '20px',
         transform: 'translate(0%, 0%)'
     }
-    function handleSubmit(e) {
+
+    let searchQuery = ""; // To store the user's input
+
+    async function handleSubmit(e) {
         e.preventDefault();
         Object.assign(searchBar.style, searchActiveStyles)
         loadPlaylistLabel.style.display = "none";
+
+        // Sending a POST request to /api/songs with the search query
+        try {
+            const response = await fetch('/api/songs', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ query: searchQuery }) // Sending the search query as a JSON object
+            });
+
+            const data = await response.json();
+
+            // You can now handle the data (e.g., render the results on the page)
+            console.log(data);
+
+        } catch (error) {
+            console.error("Error fetching songs:", error);
+        }
     }
 </script>
 
 <!--Searchbar Wrapper-->
 <form bind:this={searchBar} on:submit={handleSubmit} class="flex duration-500 ease left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 justify-between items-center fixed min-w-[300px] w-[90%] text-[12.5px] z-[100] text-text max-w-[750px]">
     <!--Search input-->
-    <input placeholder="What would you like to listen to?" class="flex justify-between items-center md:text-2xl pl-[40px] md:pl-[60px] px-[20px] py-[10px] rounded-xl backdrop-blur-xl blurback  w-full relative">
+    <input placeholder="What would you like to listen to?" class="flex justify-between items-center md:text-2xl pl-[40px] md:pl-[60px] px-[20px] py-[10px] rounded-xl backdrop-blur-xl blurback  w-full relative" bind:value={searchQuery} >
     <!--search icon-->
     <SearchIcon />
     <!--load playlist icon-->
--- src/routes/api/songs/+server.js
+++ src/routes/api/songs/+server.js
@@ -1,6 +1,113 @@
 import { error } from '@sveltejs/kit';
-/** @type {import('./$types').RequestHandler} */export function GET({ url }) {	const min = Number(url.searchParams.get('min') ?? '0');	const max = Number(url.searchParams.get('max') ?? '1');
-	const d = max - min;
-	if (isNaN(d) || d < 0) {		throw error(400, 'min and max must be numbers, and min must be less than max');	}
-	const random = min + Math.random() * d;
-	return new Response(String(random));}
+import OpenAI from 'openai';
+import { OPENAI_KEY } from "$env/static/private"
+
+const openai = new OpenAI(OPENAI_KEY);
+
+const example = `[
+    {
+        "title": "Eye of the Tiger",
+        "artist": "Survivor",
+        "reasoning": "The energetic beats and motivating lyrics of 'Eye of the Tiger' make it a classic choice for getting pumped up during a workout."
+    },
+    {
+        "title": "Lose Yourself",
+        "artist": "Eminem",
+        "reasoning": "The powerful lyrics and driving beats of 'Lose Yourself' provide the motivation and energy needed for an intense workout session."
+    },
+    {
+        "title": "Uptown Funk",
+        "artist": "Mark Ronson ft. Bruno Mars",
+        "reasoning": "This song's catchy rhythm and high-energy vibe can elevate the mood and keep you moving during your exercise."
+    },
+    {
+        "title": "Stronger",
+        "artist": "Kanye West",
+        "reasoning": "With its sample from Daft Punk's 'Harder, Better, Faster, Stronger', this song has a great tempo and beat to match the pace of a workout."
+    }
+]`
+
+async function getSpotifyTrackDetails(title, artist) {
+    const BASE_URL = 'https://api.spotify.com/v1';
+
+    // Authenticate and get an access token
+    const authResponse = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET)
+        },
+        body: 'grant_type=client_credentials'
+    });
+
+    const authData = await authResponse.json();
+    const accessToken = authData.access_token;
+
+    // Search for the track
+    const searchResponse = await fetch(`${BASE_URL}/search?q=track:${encodeURIComponent(title)} artist:${encodeURIComponent(artist)}&type=track&limit=1`, {
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        }
+    });
+
+    const searchData = await searchResponse.json();
+    const track = searchData.tracks.items[0];
+
+    if (!track) return null; // No track found
+
+    return {
+        title: track.name,
+        artist: track.artists[0].name,
+        coverArt: track.album.images[0].url, 
+        spotifyLink: track.external_urls.spotify
+    };
+}
+
+
+export function POST({ request }) {
+    const { prompt } = request.body;
+
+    const completion = openai.chat.completions.create({
+        messages: [
+            { 
+                role: "system", 
+                content: "You are GPTune - a song recommendation system which aims to provide the users with the best recommendations based on their query. Please return your answers as a json array of length 4 with the properties: title, artist, reasoning" 
+            }, 
+            { 
+                role: "user", 
+                content: "Recommend a song that's perfect for a workout session." 
+            },
+            { 
+                role: "assistant", 
+                content: example
+            }, 
+            {
+                role: "user",
+                content: prompt
+            }
+        ],
+        model: "gpt-3.5-turbo",
+    });
+
+    return completion.then(async (res) => {
+        const recommendations = JSON.parse(res.data.choices[0].text);
+
+        // Fetch Spotify details for each song recommendation
+        const detailedRecommendations = await Promise.all(recommendations.map(async song => {
+            const spotifyDetails = await getSpotifyTrackDetails(song.title, song.artist);
+            return {
+                ...song,
+                ...spotifyDetails
+            };
+        }));
+
+        return {
+            body: detailedRecommendations
+        };
+    }).catch((err) => {
+        return {
+            status: 500,
+            body: err
+        };
+    }); 
+}
