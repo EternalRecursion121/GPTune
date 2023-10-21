e6261368ad2d1e41cfceed2853d0cd5091dea165
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-10-21 13:03:50 +0100

    spotify?

---
+++ src/lib/components/Login.svelte
@@ -0,0 +1,5 @@
+<script>
+    import { signIn } from "@auth/sveltekit/client";
+</script>
+
+<button class="text-[#FFFFFF] text-6xl" on:click={() => signIn("spotify")}>SIGN IN WITH SPOTIFY</button>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { page } from "$app/stores"
+  import Login from "$lib/components/Login.svelte"
+</script>
+
+{#if $page.data.session}
+  <!-- User is logged in -->
+{:else}
+  <Login />
+{/if}
