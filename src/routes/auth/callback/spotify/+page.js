95127e317a5b205541133eae0827af922c038b24
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-10-21 18:09:06 +0100

    please work now

--- src/routes/auth/callback/spotify/+page.js
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@sveltejs/kit';
-
-export const load = ({ url }) => {
-    const code = url.searchParams.get("code");
-    console.log(code);
-
-	return redirect(300, "/")
-};
---
+++ src/routes/auth/callback/spotify/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { page, goto } from '$app/stores';
+
+  // Get the code from the URL
+  let code;
+  $page.query.subscribe(params => {
+    code = params.get('code');
+  });
+
+  // Save the token and redirect
+  if (code) {
+    localStorage.setItem('access_token', code);
+    goto('/');
+  }
+</script>
