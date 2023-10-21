aa2b17fd6ec24a74555ce8b4507a35023d9064e7
Bobberson3108 <69008832+Bobberson3108@users.noreply.github.com>
2023-10-21 18:04:21 +0100

    please

--- src/routes/auth/callback/spotify/+page.js
+++ src/routes/auth/callback/spotify/+page.js
@@ -1,7 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 
 export const load = ({ url }) => {
-    const { code } = url.searchParams;
+    const code = url.searchParams.get("code");
     console.log(code);
 
 	return redirect(300, "/")
